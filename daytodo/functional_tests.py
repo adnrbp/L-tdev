5cab37dddd1b5f74d513b80b1bfb1ea76361d7e3
adnrbp <adnrbp@users.noreply.github.com>
2019-01-17 18:52:27 -0500

    describe initial functional test

--- daytodo/functional_tests.py
+++ daytodo/functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+#Go to homepage
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+#page title and header say: "to-do Lists"
+assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+#enter a to-do item straight away
+
+#types an item in text box (hobby)
+#hits enter, page updates, page lists: 1:"item"
+
+
+
+#textbox: add anorther item. he types another.
+#page updates, show both items in list
+
+#site remember items? generated unique URL for him
+#visit and they are still there.
+
+#exits site
+browser.quit()
