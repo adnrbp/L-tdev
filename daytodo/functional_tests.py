ca0b25f65b14222b4325dadc29fceb2b988a5e40
adnrbp <adnrbp@users.noreply.github.com>
2019-01-17 19:17:41 -0500

    using unittest class to improve Functional Tests

--- daytodo/functional_tests.py
+++ daytodo/functional_tests.py
@@ -1,25 +1,32 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+	def tearDown(self):
+		#exits site
+		self.browser.quit()
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		#Go to homepage
+		self.browser.get('http://localhost:8000')
 
-#Go to homepage
-browser.get('http://localhost:8000')
+		#page title and header say: "to-do Lists"
+		#incorporates the print of actual browser.title
+		self.assertIn('To-Do', self.browser.title)
+		self.fail("Finish the test!")
+		#enter a to-do item straight away
 
-#page title and header say: "to-do Lists"
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
-
-#enter a to-do item straight away
-
-#types an item in text box (hobby)
-#hits enter, page updates, page lists: 1:"item"
+		#types an item in text box (hobby)
+		#hits enter, page updates, page lists: 1:"item"
 
 
 
-#textbox: add anorther item. he types another.
-#page updates, show both items in list
+		#textbox: add anorther item. he types another.
+		#page updates, show both items in list
 
-#site remember items? generated unique URL for him
-#visit and they are still there.
+		#site remember items? generated unique URL for him
+		#visit and they are still there.
 
-#exits site
-browser.quit()
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
