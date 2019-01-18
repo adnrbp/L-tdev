3a0aaf9b91aa3425be333c09565d1586b7b8f584
adnrbp <adnrbp@users.noreply.github.com>
2019-01-17 20:33:17 -0500

    Unit Test and map url to dummy view

--- daytodo/djangoapps/lists/tests.py
+++ daytodo/djangoapps/lists/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from djangoapps.lists.views import home_page
 
-class SmokeTest(TestCase):
-	def test_bad_maths(self):
-		self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/') #function to map
+		self.assertEqual(found.func,home_page) #match function called and the one that should be.
+
